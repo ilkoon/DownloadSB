246b44c85419663451527bfc3596eaf7b240f7ae
ilkoon <ilkon1gonfriz@gmail.com>
2024-12-23 13:27:47 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,3 +20,67 @@ function copyToClipboard() {
     });
 }
 
+
+function copyToClipboard1() {
+    const text = document.getElementById('copyText1').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
+
+function copyToClipboard2() {
+    const text = document.getElementById('copyText2').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
+
+function copyToClipboard3() {
+    const text = document.getElementById('copyText3').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
+
+
+function copyToClipboard4() {
+    const text = document.getElementById('copyText4').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
+
+function copyToClipboard5() {
+    const text = document.getElementById('copyText5').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
+
+function copyToClipboard6() {
+    const text = document.getElementById('copyText6').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
+
+function copyToClipboard7() {
+    const text = document.getElementById('copyText7').textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Текст скопирован в буфер обмена!');
+    }).catch(err => {
+        console.error('Ошибка при копировании: ', err);
+    });
+}
